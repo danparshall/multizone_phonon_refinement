c0daa69d598af70d2b97474360f7bfb26c392aab
Dan Parshall <parshall@nist.gov>
2016-04-20 18:20:49 -0400

    delete files

--- calc_JAC_multiQ.m
+++
@@ -1,3 +0,0 @@
-function jout=calc_JAC_multiQ(SYMDAT, dummy1, varsin,dummy2,dummy3)
-[funcout,jout]=calc_DAT_multiQ(SYMDAT,varsin);
-
--- remove_degenerate.m
+++
@@ -1,8 +0,0 @@
-function refinedCenters = remove_degenerate(rawCenters)
-
-refinedCenters = [];
-while length(rawCenters) != 0
-	refinedCenters = [refinedCenters rawCenters(1)];
-	mask = find(rawCenters != rawCenters(1));
-	rawCenters = rawCenters(mask);
-end
