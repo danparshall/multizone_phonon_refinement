cd3db271187f37819f72248ee54878172d3e4e02
dan <parshall.dan@gmail.com>
2022-01-26 21:05:11 -0500

    update indexing using vars_mask

--- 0_subs/update_AUX.m
+++ 0_subs/update_AUX.m
@@ -1,24 +1,24 @@
 function SYMS=update_AUX(SYMS,varsin);
-% update values from SYMS.allvars (accessed by leasqr) to various AUX.auxvars (used by "calc_DAT")
 
-%SYMS{1}.VARS.allvars
-% update VARS.allvars
+
 VARS=SYMS{1}.VARS;
 VARS.allvars(VARS.indfree)=varsin;
 
-% update AUX.auxvars
-Nqs=[0 VARS.Nqs];	% leading 0 for indexing  VARS.Nqs is is number in each AUXVARS
 
-cNqs=cumsum(Nqs);
 
 for ind=1:length(SYMS)
-	SYMS{ind}.AUX.auxvars(:,1,:) = VARS.allvars(:,1,:);
-%1+[1:Nqs(ind+1)]
-%1+[cNqs(ind)+1:cNqs(ind+1)]
-	SYMS{ind}.AUX.auxvars(:, 1+[1:Nqs(ind+1)], :) = VARS.allvars(:, 1+[cNqs(ind)+1:cNqs(ind+1)], :);
-	if length([1:Nqs(ind+1)]) ~= length([cNqs(ind)+1:cNqs(ind+1)]); disp(' oops');end
-
-%chk=SYMS{ind}.AUX.auxvars(:,:,1);
-%pause
+	if 0  % old indexing
+	% update AUX.auxvars
+	Nqs=[0 VARS.Nqs];	% leading 0 for indexing  VARS.Nqs is is number in each AUXVARS
+	cNqs=cumsum(Nqs);
+		SYMS{ind}.AUX.auxvars(:,1,:) = VARS.allvars(:,1,:);
+		SYMS{ind}.AUX.auxvars(:, 1+[1:Nqs(ind+1)], :) = VARS.allvars(:, 1+[cNqs(ind)+1:cNqs(ind+1)], :);
+		if length([1:Nqs(ind+1)]) ~= length([cNqs(ind)+1:cNqs(ind+1)]); disp(' oops'); end
+	else
+		freevars = SYMS{ind}.AUX.freevars;
+%		auxvars = SYMS{ind}.AUX.auxvars;
+		vars_mask = SYMS{ind}.AUX.vars_mask;
+		SYMS{ind}.AUX.auxvars(find(freevars)) = VARS.allvars(vars_mask(find(freevars)));
+	end
 end
 SYMS{1}.VARS=VARS;
