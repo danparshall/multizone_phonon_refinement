2f356e36080f73b6cb0317c34b194f419793aa2d
dan <parshall.dan@gmail.com>
2022-02-13 21:10:47 -0500

    create separate loop for updating freevars; move some stuff into debugging mode

--- assemble_VARS.m
+++ assemble_VARS.m
@@ -25,40 +25,52 @@ function SYMS=assemble_VARS(SYMS);
 % each SYM.
 
 
-debug = 1;
+debug = 0;
 
 n_sym = length(SYMS);
 n_cen = size(SYMS{1}.AUX.freevars, 1) - 1;
 SYMS{1}.VARS.Nph = n_cen;
 
+%%%% Here we check across all SYMS: in order to fit centers/widths, the peak has to be fittable SOMEWHERE
 poss_cen_fitting = zeros(n_cen, 1);
 for i_sym = 1:n_sym
-    % In order to fit centers/widths, the peak has to be fittable SOMEWHERE
     this_poss = sum(SYMS{i_sym}.AUX.freevars(1:n_cen, 2:end, 1), 2) > 0;
     poss_cen_fitting = poss_cen_fitting + this_poss;
 end  % end cen-fitting loop
 poss_cen_fitting = poss_cen_fitting > 0;
 
-if length(find(poss_cen_fitting)) ~= n_cen
-    disp("WARNING : some phonon peaks not fittable in any zone; these are being forced to non-free");
-    for i_sym = 1:n_sym
+for i_sym = 1:n_sym
+    if (length(find(poss_cen_fitting)) ~= n_cen)
+        if  (i_sym == 1);
+            disp("WARNING : some phonon peaks not fittable in any zone; these are being forced to non-free");
+        end;
         SYMS{i_sym}.AUX.freevars(1:n_cen, 1, 1) = poss_cen_fitting .* SYMS{i_sym}.AUX.freevars(1:n_cen, 1, 1);
         SYMS{i_sym}.AUX.freevars(1:n_cen, 1, 2) = poss_cen_fitting .* SYMS{i_sym}.AUX.freevars(1:n_cen, 1, 2);
+        if debug; disp(SYMS{i_sym}.AUX.freevars); end;
     end 
+
+    %% make mapping for jacobian indices.  (we do it here rather than "generate_AUX", since we may be changing freevars above)
+    % mapping from fitted variables to the indices corresponding to those variables within AUX.auxjac (fulljac calculated elsewhere)
+    shape_fvars = size(SYMS{i_sym}.AUX.freevars);
+    SYMS{i_sym}.AUX.jac_inds = reshape([1:prod(shape_fvars)], shape_fvars) .* SYMS{i_sym}.AUX.freevars;
+
+    % mapping from energy points contributing to the fitting, to their indices
+    shape_eng = size(SYMS{i_sym}.AUX.mask);
+    SYMS{i_sym}.AUX.eng_inds = reshape([1:prod(shape_eng)], shape_eng) .* SYMS{i_sym}.AUX.mask;
 end
 
 
+%%%% Given our updated SYMS, create the composite VARS
+
 nQs = [];   % number of Q-points for which each SYM has data
 nEs = [];   % length of energy array for each SYM
-%nFree = [];   % number of free vars for each SYM (excluding the centers & widths)
-%nData = [];
 y_obs = [];
 w_obs = [];
 jac_nnz = [];
 alt_nnz = [];
 
 cols_prev = 2*n_cen;			% the first 2*n_cen cols of the full jacobian are reserved for cens & wids
-for i_sym = 1:n_sym
+for i_sym = 1:n_sym;
     if debug; disp(''); disp(["Assembling VARS using data from SYM : " num2str(i_sym)]); end;
 
     AUX = SYMS{i_sym}.AUX;
@@ -78,7 +90,7 @@ for i_sym = 1:n_sym
     nEs = [nEs nE];
 
     if i_sym == 1
-%        disp('initializing...')
+        if debug; disp('initializing VARS...'); end;
         % the first 2*Nph elements of VARS are cens & wids
         bounds_L = [AUX.bounds_L(1:end-1, 1, :)]; bounds_L = bounds_L(:);
         bounds_H = [AUX.bounds_H(1:end-1, 1, :)]; bounds_H = bounds_H(:);
@@ -91,7 +103,7 @@ for i_sym = 1:n_sym
     % for every element of freevars, identify the corresponding column index of the full jacobian
     [vars_mask, aux_nnz] = make_vars_mask(AUX, cols_prev);
     SYMS{i_sym}.AUX.vars_mask = vars_mask;
-    SYMS{i_sym}.AUX.jac_nnz = aux_nnz;
+    SYMS{i_sym}.AUX.aux_nnz = aux_nnz;
 	cols_added = 2 * AUX.Nq * (AUX.Nph + 1);
     cols_prev = cols_prev + cols_added;
 
@@ -106,40 +118,40 @@ for i_sym = 1:n_sym
     tmp= AUX.auxvars(:, 2:end, :);
     allvars = [allvars; tmp(:)];
     prev_vars = [prev_vars, length(freevars)];
-    jac_nnz = [jac_nnz, SYMS{i_sym}.AUX.jac_nnz];
+    jac_nnz = [jac_nnz, SYMS{i_sym}.AUX.aux_nnz];
 
-
-
-    shape_fvars = size(SYMS{i_sym}.AUX.freevars);
-    SYMS{i_sym}.AUX.jac_inds = reshape([1:prod(shape_fvars)], shape_fvars) .* SYMS{i_sym}.AUX.freevars;
-
-    shape_eng = size(SYMS{i_sym}.AUX.mask);
-    SYMS{i_sym}.AUX.eng_inds = reshape([1:prod(shape_eng)], shape_eng) .* SYMS{i_sym}.AUX.mask;
-    
 
     % alternate method to determine NNZ per SYM
-    alt = 0;
-    for iq = 1:nQ
-        free_eng = AUX.mask(:,iq);
-        free_vars = [reshape(AUX.freevars(:, 1, :), [], 1); reshape(AUX.freevars(:, 1+iq, :), [], 1)]';
-        alt = alt + length(find(free_eng * free_vars));
-    end
-    alt_nnz = [alt_nnz, alt];
+    if debug
+        alt = 0;
+        for iq = 1:nQ
+            free_eng = AUX.mask(:,iq);
+            free_vars = [reshape(AUX.freevars(:, 1, :), [], 1); reshape(AUX.freevars(:, 1+iq, :), [], 1)]';  % column vector
+            alt = alt + length(find(free_eng * free_vars));
+        end
+        alt_nnz = [alt_nnz, alt];
+    end % end alt-NNZ calculator
 end  % end first SYM loop
-disp(["VARS.jac_nnz :", num2str(jac_nnz)])
-disp(["VARS.alt_nnz :", num2str(alt_nnz)])
-disp(["VARS freevars has length " num2str(length(freevars)) ", with " num2str(length(find(freevars))) " fittable"]);
+
+assert(sum(nQs .* nEs) == length(y_obs), "The output function seems to be the wrong length.")
+
+if debug
+    disp(["VARS.jac_nnz : ", num2str(jac_nnz)])
+    disp(["VARS.alt_nnz : ", num2str(alt_nnz)])
+    disp(["VARS freevars has length " num2str(length(freevars)) ", with " num2str(length(find(freevars))) " fittable"]);
+end
 
 SYMS{1}.VARS.allvars = allvars;
 SYMS{1}.VARS.bounds_L = bounds_L;
 SYMS{1}.VARS.bounds_H = bounds_H;
 SYMS{1}.VARS.freevars = freevars;
 SYMS{1}.VARS.indfree = find(freevars);
-SYMS{1}.VARS.nDatas = nQs .* nEs;
+SYMS{1}.VARS.varsin = SYMS{1}.VARS.allvars(SYMS{1}.VARS.indfree);
+SYMS{1}.VARS.nQs = nQs;
+SYMS{1}.VARS.nEs = nEs;
 SYMS{1}.VARS.y_obs = y_obs;
 SYMS{1}.VARS.w_obs = w_obs;
 SYMS{1}.VARS.prev_vars = prev_vars;
 SYMS{1}.VARS.jac_nnz = jac_nnz;
-SYMS{1}.VARS.alt_nnz = alt_nnz;
-SYMS{1}.VARS.varsin = SYMS{1}.VARS.allvars(SYMS{1}.VARS.indfree);
-assert(sum(SYMS{1}.VARS.nDatas) == length(y_obs), "The output function seems to be the wrong length.")
+if debug; SYMS{1}.VARS.alt_nnz = alt_nnz; end;
+%assert(sum(SYMS{1}.VARS.nDatas) == length(y_obs), "The output function seems to be the wrong length.")
