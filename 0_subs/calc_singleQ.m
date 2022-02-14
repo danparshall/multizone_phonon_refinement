526eeaf212622160a71d698d6c894407e8d8ebba
dan <parshall.dan@gmail.com>
2022-02-13 21:17:32 -0500

    clean up

--- 0_subs/calc_singleQ.m
+++ 0_subs/calc_singleQ.m
@@ -1,5 +1,5 @@
-function [modelout,jacout,varargout]=calc_singleQ(AUX,idx);
-% [modelout,jacout,varargout]=calc_singleQ(AUX,DAT,idx);
+function [modelout,jac_out,varargout]=calc_singleQ(AUX,idx);
+% [modelout,jac_out,varargout]=calc_singleQ(AUX,DAT,idx);
 % 	Calculates the data for a single Q.  
 %	If requested, calculates and delivers Jacobian as well.
 %	Cycles through, calculates profile and jacobian for each phonon.
@@ -8,8 +8,8 @@ if ~exist('idx'); idx=1; end
 
 eng = AUX.eng(AUX.mask(:,idx))';     % row vector of just energies being fitted
 
-cen=AUX.auxvars(1:end-1, 1, 1);
-ht=AUX.auxvars(1:end-1, idx+1, 1);
+cen = AUX.auxvars(1:end-1, 1, 1);
+hts = AUX.auxvars(1:end-1, idx+1, 1);
 wid = AUX.auxvars(1:end-1, 1, 2);
 res = AUX.auxvars(1:end-1, 1+idx, 2);
 
@@ -21,12 +21,12 @@ w1 = FWHM * asymm/(asymm+1);
 w2 = FWHM * 1/(asymm+1);
 
 
-
 % Background
 constant = AUX.auxvars(end,idx+1,1);
 slope = AUX.auxvars(end,idx+1,2);
 
-%%% INIT
+
+%%%% INIT
 modelout = zeros(size(eng));
 
 if nargout==2;
@@ -37,34 +37,26 @@ if nargout==2;
 	%	heights + constant BG
 	%	reswids + linear BG
 	vsize = size(AUX.freevars, 1);
-	jacout = zeros(length(eng), 4*vsize);
-	jacout(:, 3*vsize) = ones(length(eng),1);	% const BG
-	jacout(:, 4*vsize) = eng(:);				% linear BG
-
-%	jacout=[zeros(length(eng),3*AUX.Nph) ones(length(eng),1) eng(:)];  % last two cols are derivatives of background
+	jac_out = zeros(length(eng), 4*vsize);
+	jac_out(:, 3*vsize) = ones(length(eng),1);	% const BG
+	jac_out(:, 4*vsize) = eng(:);				% linear BG
 end
 
 
+%%%% Calculate profile for each phonon
 for ind=1:AUX.Nph
 	if nargout==2	% when asked for jacobian
-%		[splitgauss,splitjac] = calc_splitgauss_JAC_fast(eng,cen(ind),ht(ind),w1(ind),w2(ind));
-		[splitgauss, splitjac] = calc_splitgauss_full(eng, cen(ind), ht(ind), wid(ind), res(ind), asymm );
-		modelout = modelout + splitgauss';
-        
-%		ind_j = (3*ind)-2;
-%		jacout(:,[ind_j:ind_j+2])=splitjac;     	% cen ht wid (both widths combined)
+		[splitgauss, splitjac] = calc_splitgauss_full(eng, cen(ind), hts(ind), wid(ind), res(ind), asymm );
 		ind_cwh = [0, 2*vsize, vsize] + ind;  	% resorts so that first Nph cols are cens, then wids, then heights
-		jacout(:, ind_cwh) = splitjac;
+		jac_out(:, ind_cwh) = splitjac;
 
 	else			% no jacobian
-%		splitgauss = calc_splitgauss_JAC_fast(eng,cen(ind),ht(ind),w1(ind),w2(ind));
-		splitgauss = calc_splitgauss_full(eng, cen(ind), ht(ind), wid(ind), res(ind), asymm );
-		modelout = modelout + splitgauss';
+		splitgauss = calc_splitgauss_full(eng, cen(ind), hts(ind), wid(ind), res(ind), asymm );
 	end
-end
 
+	modelout = modelout + splitgauss';
+end
 
 % tack on the backgrounds to the convolved splitgausses
 modelout = modelout + constant;		% constant BG
 modelout = modelout + (slope*eng);	% linear BG
-
