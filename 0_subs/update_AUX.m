ec6bd288e0a3627af035d7f241fd926492b91be9
dan <parshall.dan@gmail.com>
2022-02-13 21:16:36 -0500

    delete old indexing

--- 0_subs/update_AUX.m
+++ 0_subs/update_AUX.m
@@ -1,24 +1,13 @@
-function SYMS=update_AUX(SYMS,varsin);
+function SYMS = update_AUX(SYMS,varsin);
 
-
-VARS=SYMS{1}.VARS;
-VARS.allvars(VARS.indfree)=varsin;
-
-
+VARS = SYMS{1}.VARS;
+VARS.allvars(VARS.indfree) = varsin;
 
 for ind=1:length(SYMS)
-	if 0  % old indexing
-	% update AUX.auxvars
-	Nqs=[0 VARS.Nqs];	% leading 0 for indexing  VARS.Nqs is is number in each AUXVARS
-	cNqs=cumsum(Nqs);
-		SYMS{ind}.AUX.auxvars(:,1,:) = VARS.allvars(:,1,:);
-		SYMS{ind}.AUX.auxvars(:, 1+[1:Nqs(ind+1)], :) = VARS.allvars(:, 1+[cNqs(ind)+1:cNqs(ind+1)], :);
-		if length([1:Nqs(ind+1)]) ~= length([cNqs(ind)+1:cNqs(ind+1)]); disp(' oops'); end
-	else
-		freevars = SYMS{ind}.AUX.freevars;
-%		auxvars = SYMS{ind}.AUX.auxvars;
-		vars_mask = SYMS{ind}.AUX.vars_mask;
-		SYMS{ind}.AUX.auxvars(find(freevars)) = VARS.allvars(vars_mask(find(freevars)));
-	end
+	freevars = SYMS{ind}.AUX.freevars;
+	vars_mask = SYMS{ind}.AUX.vars_mask;
+	% the positon in vars_mask corresponds to auxvars; the value in vars_mask is the index (column of jac) that variable occupies in VARS.allvars
+	SYMS{ind}.AUX.auxvars(find(freevars)) = VARS.allvars(vars_mask(find(freevars)));
 end
-SYMS{1}.VARS=VARS;
+
+SYMS{1}.VARS = VARS;
